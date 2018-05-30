b688c09561c5a75fda79e79add3458557ff22b6b
Eric Shull and Thad Donaghue <eric@>
2018-05-30 13:05:57 -0700

    finalize flashcard page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 
-  $(".contents").click(function() {
-    $(".flashcard").toggle();
-    $(".info").toggle();
+    $(".contents").click(function() {
+      $(".flashcard", this).toggle();
+      $(".info", this).toggle();
+    });
+});
+
+/*
+$(document).ready(function() {
+  $(".contents").click() {
+    $(this).click(function() {
+      $(".flashcard").toggle();
+      $(".info").toggle();
+    });
   });
 
 });
+*/
